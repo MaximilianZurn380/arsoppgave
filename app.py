77a60ded9483f2e6f08f0ebcada41799d1fb34b9
James <mazua001@gmail.com>
2025-03-31 11:52:20 +0200

    made a log in function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from flask import Flask, render_template
+from flask import Flask, request, render_template, session
 
 
 app = Flask(__name__)
@@ -15,6 +15,34 @@ def get_db_connection():
         charset = "utf8mb4",
         collation = "utf8mb4_general_ci"
     )
+
+@app.route("/")
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        db = get_db_connection()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+
+        user = cursor.fetchone()
+
+        cursor.close()
+        db.close()
+
+        if user:
+            session.permanent = True
+            session["email"] = email
+            return redirect(url_for("mainpage"))
+        else:
+            session["email"] = email
+            return "Wrong email or password"
+    else:
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
