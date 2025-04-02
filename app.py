952bded8f82fb81e54e014cfac3633a7042dab0a
James <mazua001@gmail.com>
2025-04-02 11:38:07 +0200

    made it run

--- app.py
+++ app.py
@@ -74,3 +74,7 @@ def register():
         session["email"] = email
         return redirect(url_for("store"))
     return render_template(register.html)
+
+
+    if __name__ == "__main__":
+        app.run(debug=True)
