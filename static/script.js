dcb0dbde1c1fe9a2fc6f0186b8db7a09a39868f4
James <mazua001@gmail.com>
2025-04-08 13:02:39 +0200

    added a toggle option for a menu

--- static/script.js
+++ static/script.js
@@ -1,9 +1,23 @@
 let farge = document.body;
-
 let typer = document.getElementById("typebox");
+
+let hideMenu = document.getElementById("hidemenu");
+let toggleMenu = document.getElementById("togglemenu")
 
 document.getElementById("choose").addEventListener("click", function ()
 {
     let chosen = typer.value;
     farge.style.backgroundColor = chosen;
+});
+
+toggleMenu.addEventListener("click", function ()
+{
+    if (hideMenu.style.display === "none")
+    {
+        hideMenu.style.display = "block";
+        toggleMenu.textContent = "Close line menu";
+    } else {
+        hideMenu.style.display = "none";
+        toggleMenu.textContent = "Make a new line";
+    }
 });
